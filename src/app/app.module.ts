d1a074e1600ac3b45f4438bceaf8edb63cdac6cc
marlinayusoff <yusnamarlina@gmail.com>
2018-04-30 13:05:15 +0800

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
 
 import { MyApp } from './app.component';
+import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
+import { LoginPage } from '../pages/login/login';
 import { RestProvider } from '../providers/rest/rest';
 
 @NgModule({
   declarations: [
     MyApp,
+    LoginPage,
+    TabsPage,
     HomePage
   ],
   imports: [
     BrowserModule,
+    HttpModule,
+    IonicStorageModule.forRoot(),
     IonicModule.forRoot(MyApp)
+    
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
+    LoginPage,
+    TabsPage,
     HomePage
   ],
   providers: [
--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { Component,ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { RestProvider } from '../../providers/rest/rest';
 
 @Component({
   selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  loginData:any;
+	data:any;
+	@ViewChild('username') username;
+	@ViewChild('password') password; 
 
-  constructor(public navCtrl: NavController) {
-
+  constructor(public navCtrl: NavController, public navParams: NavParams, private alertCtrl: AlertController, public restProvider: RestProvider) {
   }
+  
+  presentAlert(title,subTitle) {
+    let alert = this.alertCtrl.create({
+        title: title,
+        subTitle: subTitle,
+        buttons: ['Dismiss']
+      })
+      alert.present();
+    }
+  
+  
+    tryLogin(){
+      if(this.username.value !== "" && this.password.value !== "")
+      {
+        this.loginData = {
+          username: this.username.value,
+          password: this.password.value
+        };
+          this.restProvider.login(this.loginData);
+      }
+      else
+      {
+        this.presentAlert('Error','Please fill all the inputbox');
+      }
+  
+    }
 
 }
